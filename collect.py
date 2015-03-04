0a831ea14b9e03591db956ebc33dfbbb4bdb961b
Doron Richartz <Doron.Richartz@Webtrends.com>
2015-03-04 14:00:08 -0800

    cleaned up

--- collect.py
+++ collect.py
@@ -1,4 +1,3 @@
-import dbinteract
 import feedparser
 import urllib2
 from bs4 import BeautifulSoup
@@ -15,7 +14,6 @@ couch = Server('http://192.168.59.103:49153/')
 dbv = couch['untappd_venue']
 dbb = couch['untappd_beer']
 venue_list = ["https://untappd.com/rss/venue/3538"]
-venue_name = ["Saraveza"]
 
 for i in venue_list:
     feed = feedparser.parse(i)
@@ -28,20 +26,26 @@ for i in range(len(feed['entries'])):
     print "Getting Untappd checkin:"
     print feed['entries'][i]['links'][0]['href']
     web_page = urllib2.urlopen(feed['entries'][i]['links'][0]['href'])
-    #print web_page
-#grab timestamp?
     soup = BeautifulSoup(web_page)
     c = soup.find_all("div", class_="beer")
     for div in c:
-        poop = div.find_all('a')
-    for i in range(len(poop)):
+        checkin_data = div.find_all('a')
+    for i in range(len(checkin_data)):
         if i % 2 == 0:
-            e['beer'] = (poop[i].get_text())
+            e['beer'] = (checkin_data[i].get_text())
         else:
-            e['brewery'] = (poop[i].get_text())
+            e['brewery'] = (checkin_data[i].get_text())
     doc_id, doc_rev = dbv.save(e)
     print doc_id, doc_rev
 
 for docid in dbv:
   e = dbv.get(docid)
   print e['brewery'], e['beer'], e['venue']
+
+beer_query = "Pliny The Younger"
+code = '''function(doc) { if(doc.beer == '%s') emit(doc.frm, null); }''' % beer_query
+for res in dbv.query(code):
+    g = dbv.get(res.id)
+    print g['beer'] + " is at " + g['venue']
+
+
